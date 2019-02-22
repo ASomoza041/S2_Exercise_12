cc4766e2b55c0ef5b5c9f601ea23eb50f5b24aa9
Alex Somoza <ASomoza041@west-mec.org>
2019-02-21 18:25:12 -0700

    Project Update
    Created variables and return values to display the date in the caption of the table.

--- lht_calendar.js
+++ lht_calendar.js
@@ -38,6 +38,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 // Function to generate the calendar table
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
+      calendarHTML += calCaption(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -46,4 +47,14 @@ function createCalendar(calDate) {
 function calCaption(calDate) {
       //month name array contains the list of month names
       var monthName = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+      //determine the current month
+      var thisMonth = calDate.getMonth();
+
+      //determine current year
+      var thisYear = calDate.getFullYear();
+
+      //Write the caption
+      return "<caption>" + monthName[thisMonth] + " " +
+            thisYear + "</caption>";
 }
